d020325e08e758d43d97e461177eabbf39213f03
Ádám <dm@DESKTOP-D2T7IC6>
2021-10-01 10:42:16 +0200

    Create default UI

---
+++ UserMaintenance/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+    class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Fullname
+        {
+            get { return string.Format("{0} {1}", Lastname, Fullname); }
+        }
+
+    }
+}
