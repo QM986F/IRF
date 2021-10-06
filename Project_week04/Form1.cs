197e1d8a448c115bbe6df8d13a1818dc70ab67d8
Ádám <dm@DESKTOP-D2T7IC6>
2021-10-06 12:24:03 +0200

    Create and Open Excel Workbook

--- Project_week04/Form1.cs
+++ Project_week04/Form1.cs
@@ -16,15 +16,48 @@ namespace Project_week04
     {
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWb = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWb.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+            }
+        }
+        private void CreateTable()
+        {
+
         }
     }
 }
