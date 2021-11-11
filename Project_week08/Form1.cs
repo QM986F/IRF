d74ff0346902611558b40f47719b9d2f88119944
Ádám <dm@DESKTOP-D2T7IC6>
2021-11-11 13:56:13 +0100

    Create Conveyor belt

--- Project_week08/Form1.cs
+++ Project_week08/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_week08.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,47 @@ namespace Project_week08
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            int right = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left>right)
+                {
+                    right = ball.Left;
+                }
+            }
+
+            if (right>1000)
+            {
+                var oldestBall = _balls[0];
+                _balls.Remove(oldestBall);
+                mainPanel.Controls.Remove(oldestBall);
+            }
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
         }
     }
 }
