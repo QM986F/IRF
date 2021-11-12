8ad421c11054636d9c78d332226004b9e226fb03
Ádám <dm@DESKTOP-D2T7IC6>
2021-11-12 23:29:52 +0100

    Ball coloring

--- Project_week08/Form1.cs
+++ Project_week08/Form1.cs
@@ -64,7 +64,10 @@ namespace Project_week08
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory
+            {
+                BallColor = button3.BackColor
+            };
         }
 
         private void button1_Click(object sender, EventArgs e)
