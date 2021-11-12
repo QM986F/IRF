0831c56f1a22d62c073f3fd68af78292814f0e07
Ádám <dm@DESKTOP-D2T7IC6>
2021-11-12 22:13:11 +0100

    Factory abstraction management

---
+++ Project_week08/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using Project_week08.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_week08.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
