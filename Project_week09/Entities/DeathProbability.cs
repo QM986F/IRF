d37e56a331c70cafba746c27ce4c482f9b4bd398
Ádám <dm@DESKTOP-D2T7IC6>
2021-12-03 23:36:58 +0100

    Loading Dates

--- Project_week09/Entities/DeathProbability.cs
+++ Project_week09/Entities/DeathProbability.cs
@@ -8,8 +8,8 @@ namespace Project_week09.Entities
 {
     public class DeathProbability
     {
-        public int BirthYear { get; set; }
         public Gender Gender { get; set; }
+        public int BirthYear { get; set; }
         public double DProbability { get; set; }
     }
 }
