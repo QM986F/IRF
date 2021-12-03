8b154f930959deac860b809e31696a0631469c3a
Ádám <dm@DESKTOP-D2T7IC6>
2021-12-03 23:13:25 +0100

    Create Classes

---
+++ Project_week09/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_week09.Entities
+{
+    public class DeathProbability
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public double DProbability { get; set; }
+    }
+}
